36fe0de7fc4ec00e3e000d113ed1f94a25edc441
yuval-hazaz <hazaz.yuval@gmail.com>
2023-07-12 19:49:42 +0300

    fix deps

--- apps/last-version-auth/src/mor/mor.module.ts
+++ apps/last-version-auth/src/mor/mor.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { MorModuleBase } from "./base/mor.module.base";
 import { MorService } from "./mor.service";
 import { MorController } from "./mor.controller";
 import { MorResolver } from "./mor.resolver";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-  imports: [MorModuleBase],
+  imports: [MorModuleBase, forwardRef(() => AuthModule)],
   controllers: [MorController],
   providers: [MorService, MorResolver],
   exports: [MorService],
